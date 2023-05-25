919753bfc163cc921df5ea8c7e1d1281b6681da4
rupambora <rupambora13@gmail.com>
2023-05-25 18:21:07 +0530

    App code updated

--- lab/lab 7 - IoT based sensor monitoring and actuation app/IBSMAA_data_collection/IBSMAA_data_collection.ino
+++ lab/lab 7 - IoT based sensor monitoring and actuation app/IBSMAA_data_collection/IBSMAA_data_collection.ino
@@ -1,21 +1,17 @@
 #include <WiFi.h>
 #include <Wire.h>
-#include "Adafruit_HTU21DF.h"
-#include "Adafruit_BMP085.h"
-
-Adafruit_BMP085 bmp;
-
 
 #define WIFI_NETWORK "IoT Project"
 #define WIFI_PASSWORD "12345678"
 #define WIFI_TIMEOUT_MS 20000
-#define seaLevelPressure_hPa 1013.25
 
-const int line1 = 26;
-const int line2 = 25;
+int ledPin = 13;
+bool ledState = LOW;
 const int trigPin = 5;
 const int echoPin = 18;
 int duration, distance;
+WiFiServer server(80);
+
 void connectToWiFi(){
     Serial.print("Connecting to Wifi");
     WiFi.mode(WIFI_STA);
@@ -34,16 +30,21 @@ void connectToWiFi(){
      else {
         Serial.print("Connected");
         Serial.println(WiFi.localIP());
+        //digitalWrite(LED_BUILTIN, HIGH);
       }
   }
+
 
 void setup() {
   Serial.begin(115200);
   connectToWiFi();
-  pinMode(line1, OUTPUT);
-  pinMode(line2, OUTPUT);
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
 }
 
 void loop() {
@@ -56,21 +57,32 @@ void loop() {
  digitalWrite(trigPin, LOW);
  duration = pulseIn(echoPin, HIGH);
  distance = duration*0.034/2;
- Serial.println(distance);
+ Serial.print(distance);
+ Serial.println(" cm");
  delay(100);
+ WiFiClient client = server.available();
+  if (client) {
+    while (client.connected()) {
+      String data = String(distance); // Replace with your sensor data source
 
- // BMP180
- Serial.print("Temp = ");
- Serial.print(bmp.readTemperature());
- Serial.println(" *C");
+      client.println(data);
+      delay(1000);
 
- Serial.print("Pressure = ");
- Serial.print(bmp.readPressure());
- Serial.println(" Pa");
+      String request = client.readStringUntil('\n');
 
+      if (request == "HIGH") {
+        ledState = HIGH;
+      } else if (request == "LOW") {
+        ledState = LOW;
+      }
 
- Serial.print("Altitude = ");
- Serial.print(bmp.readAltitude());
- Serial.println(" meters");
- delay(100);
+      digitalWrite(ledPin, ledState);
+
+      client.println(ledState == HIGH ? "LED ON" : "LED OFF");
+      delay(1000);
+      
+    }
+    client.stop();
+  }
+ 
 }
--- lab/lab 7 - IoT based sensor monitoring and actuation app/main.py
+++ lab/lab 7 - IoT based sensor monitoring and actuation app/main.py
@@ -1,32 +1,54 @@
-from kivy.config import Config
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.graphics import Color, Line
-
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.clock import Clock
+import socket
 
 class IoT(App):
     def build(self):
+        # Create UI
         layout = BoxLayout(orientation='vertical')
+        self.data_label = Label(text='Waiting for data...')
+        self.button_on = Button(text='LED ON')
+        self.button_off = Button(text='LED OFF')
 
-        # Add a label widget
-        label = Label(text='Hello Kivy!',
-                      font_size='50sp',
-                      size_hint=(1, 1))
-        layout.add_widget(label)
+        layout.add_widget(self.data_label)
 
-        # Add a border to the layout
-        with layout.canvas:
-            Color(1, 0, 0, 1)  # Set border color (red)
-            Line(rectangle=(0, 0, 360, 640),
-                 width=2)
+        self.button_on.bind(on_press=self.send_high)
+        self.button_off.bind(on_press=self.send_low)
 
+        layout.add_widget(self.data_label)
+        layout.add_widget(self.button_on)
+        layout.add_widget(self.button_off)
+        
+        # Open socket connection to ESP32 server
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(('ESP32_IP_ADDRESS', 80))  # Replace 'ESP32_IP_ADDRESS' with the IP address of your ESP32
+        
+        # Schedule the update function to run every second
+        Clock.schedule_interval(self.update_data, 1)
+        
         return layout
+    
+    def update_data(self, dt):
+        # Receive data from the ESP32 server
+        data = self.sock.recv(1024).decode().strip()
+        
+        # Update the UI label with the received data
+        self.data_label.text = data
 
+    def send_high(self, instance):
+        # Send "HIGH" command to the ESP32 server
+        self.sock.sendall(b"HIGH\n")
+    
+    def send_low(self, instance):
+        # Send "LOW" command to the ESP32 server
+        self.sock.sendall(b"LOW\n")
+
+    def on_stop(self):
+        # Close the socket connection when the app is closed
+        self.sock.close()
 
 if __name__ == '__main__':
-    Config.set('graphics', 'width', '360')
-    Config.set('graphics', 'height', '640')
-    Config.set('graphics', 'orientation', 'portrait')
-
     IoT().run()
