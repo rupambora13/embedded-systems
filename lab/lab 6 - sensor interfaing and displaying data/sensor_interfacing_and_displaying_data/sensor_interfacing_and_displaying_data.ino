e66fd728120aef7f6939582af585f708943dbb27
rupambora <rupambora13@gmail.com>
2023-05-31 00:09:32 +0530

    lab updated

--- lab/lab 6 - sensor interfaing and displaying data/sensor_interfacing_and_displaying_data/sensor_interfacing_and_displaying_data.ino
+++ lab/lab 6 - sensor interfaing and displaying data/sensor_interfacing_and_displaying_data/sensor_interfacing_and_displaying_data.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include "SparkFunHTU21D.h"
 
+HTU21D htu;
+
+void setup()
+{
+  Serial.begin(115200);
+  htu.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  float humd = htu.readHumidity();
+  float temp = htu.readTemperature();
+  
+  Serial.print(" Temperature:");
+  Serial.print(temp);
+  Serial.println("C");
+  Serial.print(" Humidity:");
+  Serial.print(humd);
+  Serial.println("%");
+  Serial.println();
+  delay(1000);
+ 
 }
